34149baeafa6c410b3677d068d174bc9b9cb039a
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-04-20 12:05:51 -0300

    feat: add routes to project

--- src/modules/coffee-report/coffee-report.controller.ts
+++ src/modules/coffee-report/coffee-report.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
 
-@Controller()
+@Controller('coffe-report')
 export class CoffeeReportController {}
--- src/modules/voltage-analysis-report/voltage-analysis-report.controller.ts
+++ src/modules/voltage-analysis-report/voltage-analysis-report.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
 
-@Controller()
+@Controller('voltage-analysis-report')
 export class VoltageAnalysisReportController {}
